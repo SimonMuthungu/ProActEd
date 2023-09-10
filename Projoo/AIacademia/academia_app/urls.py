996a2e808c97387781fd104a1183889f1ad2bd47
MerVitz <amakaluvitalis202@gmail.com>
2023-09-10 16:22:46 +0300

    Changes

---
+++ Projoo/AIacademia/academia_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.login, name="login"),
+    path("student_page", views.student_page, name="student_page"),
+    path("admin_page", views.admin_page, name="admin_page")
+]
