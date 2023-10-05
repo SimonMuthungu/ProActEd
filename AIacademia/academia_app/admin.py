b9491dc17034b6e6669061a032c624f7b53f0718
MerVitz <amakaluvitalis202@gmail.com>
2023-10-05 15:29:25 +0300

    MMH Still on error handling

--- AIacademia/academia_app/admin.py
+++ AIacademia/academia_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import School, Department, Course
+
+from .models import Course, Department, School
 
 admin.site.register(School)
 admin.site.register(Department)
