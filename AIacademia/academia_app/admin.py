4034c3460583a40d394b3cb61d5a2d714e658a85
MerVitz <amakaluvitalis202@gmail.com>
2023-10-05 16:58:44 +0300

    Added Departments and Schools data to the  Database

--- AIacademia/academia_app/admin.py
+++ AIacademia/academia_app/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
-from .models import Course, Department, School
+from .models import Course, Department, School, Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration_number', 'course', 'department', 'school', 'graduation_probability')
+    list_filter = ('course', 'department', 'school')
+    search_fields = ('name', 'registration_number')
+    fieldsets = (
+        ('Student Information', {
+            'fields': ('name', 'registration_number', 'graduation_probability')
+        }),
+        ('Academic Details', {
+            'fields': ('course', 'department', 'school')
+        }),
+    )
 
 admin.site.register(School)
 admin.site.register(Department)
 admin.site.register(Course)
+admin.site.register(Student, StudentAdmin)
