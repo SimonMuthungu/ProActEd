0e55f78fce2c8010634bfd5591ae35d424724633
Simon Muthungu <henrymsechu1@gmail.com>
2023-10-18 13:23:44 +0300

    few tweaks to the recommender system

--- AIacademia/python_scripts/recommender_model.py
+++ AIacademia/python_scripts/recommender_model.py
@@ -5,8 +5,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-
-
 path_to_data = r'C:\Users\Simon\proacted\AIacademia\python_scripts\preprocessed_data_two_columns.xlsx'
 
 df = pd.read_excel(path_to_data)
@@ -20,7 +18,7 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(df['Course Objectives'])
 # print(tfidf_matrix)
 
 # testing with dummy interests
-student_interests = "religion"
+student_interests = "build tech systems"
 
 # transform these using the same tf-idf
 student_profile = tfidf_vectorizer.transform([student_interests])
@@ -28,9 +26,11 @@ student_profile = tfidf_vectorizer.transform([student_interests])
 # calculating cosine similarities b2n the dataset and student interests, and give an array
 similarities = cosine_similarity(student_profile, tfidf_matrix)
 
+
 similar_courses_indices = similarities.argsort(axis=1)[:, ::-1]
 
 # print(similar_courses_indices)
+
 top_5 = 5
 recommended_courses = similar_courses_indices[0][:top_5]
 
