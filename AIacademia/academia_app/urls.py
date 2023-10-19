07268f0598e62edbd1b4e63c2cc05ab6037fc18c
MerVitz <amakaluvitalis202@gmail.com>
2023-10-19 17:45:37 +0300

    Am changing departments

--- AIacademia/academia_app/urls.py
+++ AIacademia/academia_app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("", views.login, name="login"),
     path("student_page", views.student_page, name="student_page"),
     path("admin_page", views.admin_page, name="admin_page"),
-     path('api/get_courses/<int:department_id>/', views.get_courses, name='get_courses')
+    path('api/get_courses/<int:school_id>/', views.get_courses, name='get_courses'),
 ]
+
 
 
     
