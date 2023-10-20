c52e87c5cc0e7e775836ee7232ec27653a8b630f
Simon Muthungu <henrymsechu1@gmail.com>
2023-10-20 13:12:17 +0300

    trying to push to heroku for hosting

--- AIacademia/AIacademia/settings.py
+++ AIacademia/AIacademia/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Whitenoise',
 ]
 
 ROOT_URLCONF = 'AIacademia.urls'
@@ -76,11 +78,8 @@ WSGI_APPLICATION = 'AIacademia.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) 
     }
-}
 
 
 
