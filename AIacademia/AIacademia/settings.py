5d880666634a284511c5e2faf1657b8fe888f309
Simon Muthungu <henrymsechu1@gmail.com>
2023-10-26 15:29:06 +0300

    moved excel data to dbsqlite3

--- AIacademia/AIacademia/settings.py
+++ AIacademia/AIacademia/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'AIacademia.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
+}
 
 
 
