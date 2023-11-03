feb140106e36873f8649f07768bda04c982a06a1
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-03 12:02:16 +0300

    sign up

--- AIacademia/academia_app/urls.py
+++ AIacademia/academia_app/urls.py
@@ -4,14 +4,17 @@ from .views import admin_login_view
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
+    path("", views.login_a, name="login_a"),
     path("student_page", views.student_page, name="student_page"),
     path("admin_page", views.admin_page, name="admin_page"),
     path('api/get_courses/<int:department_id>/', views.get_courses, name='get_courses'),
     path('admin-login/', views.admin_login_view, name='admin_login'),
     path("course_recommendation_page", views.course_recommendation_page, name="course_recommendation_page"),
-    path ("intervention", views.intervention, name="intervention"),
-
+    path ("intervention/", views.intervention, name="intervention"),
+    path ("signup", views.signup, name="signup"),
+    path ("intervention_page/", views.intervention_page, name="intervention_page"),
+    path ("signin", views.signin, name="signin"),
+    path ("signout", views.signout, name="signout"),
 ]
 
 
--- AIacademia/academia_app/views.py
+++ AIacademia/academia_app/views.py
@@ -1,7 +1,23 @@
+from sre_constants import BRANCH
+from telnetlib import LOGOUT
 from academia_app.models import Department
-from django.http import (  # Import JsonResponse for AJAX responses
-    HttpResponse, JsonResponse)
+# from django.http import git BRANCH(  # Import JsonResponse for AJAX responses
+#     HttpResponse, JsonResponse)
+from django.shortcuts import render, redirect
+# from .forms import UserLoginForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate ,login
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate
+# Other imports
+from django.contrib.auth import authenticate ,login , logout
+from django.contrib.auth import login
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Course, School # Import Course and School models
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Other imports
 
@@ -11,35 +27,110 @@ def department_list(request):
 def admin_login_view(request):
     return render (request ,"academia_app/ admin_login-view.html" )
 
-def login(request):
-    return render(request, "academia_app/login.html")
+def login_a(request):
+    return render(request, "academia_app/login_a.html")
 
 def student_page(request):
     return render(request, "academia_app/student_page.html")
 
 def course_recommendation_page(request):
     return render (request,"academia_app/course_recommendation_page.html")
+def intervention(request):
+    return render (request,"academia_app/intervention.html")
 
 def intervention (request):
     return render (request, "academia_app/intervention.html")
 
+def intervention_page (request):
+    return render (request, "academia_app/intervention_page.html")
+
 def admin_page(request):
     return render(request, "academia_app/admin_page.html")
 
-from .models import Course, Department, School
-
+def admin_login_view(request):
+    # Your view logic for admin login goes here
+    return render(request, "admin_login.html")  # Replace "admin_login.html" with the actual template
 
 def school_list(request):
     schools = School.objects.all()
-    departments = Department.objects.all()
-    return render(request, 'admin_page.html', {'schools': schools, 'departments': departments})
+    # Retrieve courses for all schools (you may need to adjust this based on your use case)
+    courses = Course.objects.all()
+    return render(request, 'academia_app/admin_page.html', {'schools': schools, 'courses': courses})
 
-def get_courses(request, department_id):
-    # Retrieve courses for the selected department
-    courses = Course.objects.filter(department_id=department_id).values('id', 'name')
+def get_courses(request, school_id):
+    # Retrieve courses for the selected school
+    courses = Course.objects.filter(school_id=school_id).values('id', 'name')
     
     # Convert courses to a list of dictionaries
     course_list = list(courses)
 
-    # Return courses as JSON response
+    # Return courses as a JSON response
     return JsonResponse(course_list, safe=False)
+# class CustomLoginView(LoginView):
+#     template_name = 'login.html'
+#     success_url = reverse_lazy('success_url_name')
+    
+
+# def intervention_view(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(request.POST)
+#         if form.is_valid():
+#             registration_number = form.cleaned_data['registration_number']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=registration_number, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 # Redirect to a success page or another view after successful login
+#                 return redirect('success_page')
+#             else:
+#                 # Handle invalid credentials (display error message, etc.)
+#                 pass
+#     else:
+#         form = UserLoginForm()
+#     return render(request, 'intervention.html', {'form': form})    
+
+# def intervention (request):
+#       return HttpResponse(request,"academia_app/intervention.html")
+  
+def signup (request):
+    
+    if  request.method == "POST":
+            username = request.POST['username']
+            fname = request.POST["fname"]
+            lname = request.POST["lname"]
+            email = request.POST["email"]
+            pass1= request.POST["pass1"]
+            pass2 = request.POST["pass2"]
+
+            myuser = User.objects.create_user (username, email, pass1)
+            myuser.first_name =fname
+            myuser.last_name =lname
+            myuser.save ()
+        
+            messages.success (request, "Your account has been successfully created.")
+        
+            return redirect("signin")
+    
+    return render (request, "academia_app/signup.html")
+  
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1= request.POST["pass1"]
+        user = authenticate( username=username,password=pass1 )
+        
+        if   user is not None:
+            login(request,user)
+            fname = user.first_name
+            return render(request, "academia_app/intervention.html", {"fname":fname})
+        else:
+            
+            messages.error(request, "Bad Credentials")
+            return redirect('intervention')
+    return render(request, "academia_app/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect("intervention")
