d8729b8ded805b7de4bb14e45f1a02862caa15ea
Simon Muthungu <henrymsechu1@gmail.com>
2023-11-17 14:15:36 +0300

    starting to apply clustering and pooling for a better recommendation

--- AIacademia/python_scripts/recommender_model.py
+++ AIacademia/python_scripts/recommender_model.py
@@ -1,3 +1,5 @@
+# this is the file responsible for all training of the model.
+
 import os
 import django
 import pandas as pd
@@ -7,71 +9,16 @@ import joblib
 import sys
 import gensim
 import numpy as np
+from AIacademia.python_scripts.run_recommender_system import weighted_vector, vectorizer, tfidf_matrix, tfidf_matrix_prerequisites, prerequisites_vectorizer, feature_names, feature_names_for_prerequisites, df
 from prepare_recommender_dataset import preprocess_text
 from scipy.sparse import hstack
 
-# welcome
-
-print('\n\n\n|---------------------------------------------------------------------------------------------|')
-print('|---------------------------------------------------------------------------------------------|')
-print('|                       WELCOME TO PROACTED                                                   |')
-print('|                       @ PROACTED 1.1 2023                                                   |')
-print('|---------------------------------------------------------------------------------------------|')
-print('|---------------------------------------------------------------------------------------------|')
-print('|---------------------------------------------------------------------------------------------|\n\n\n')
-
-print('\n\nEnter your interests. Feel free to talk... :)\n\n')
-user_interests = input()
-user_subjects = input('\nAnd subjects you excelled at in high school?\n\n')
-
-user_interests = preprocess_text(user_interests)
-user_subjects = preprocess_text(user_subjects)
-
-
-print(f'\n\nGive us a moment, as we give you our best...\n\n')
-
-# setting up django environment to interact with django from this script
-sys.path.append(r'C:\Users\Simon\proacted\AIacademia')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIacademia.settings')
-django.setup()
-
-# getting courses from django dbsqlite3 and making them into a df
-from academia_app.models import Recommender_training_data
-all_courses = Recommender_training_data.objects.all()
-courses_list = [{"Course Name": course.Course_name,
-                 "Combined Info": preprocess_text(course.Course_objectives) + " " + preprocess_text(course.Course_general_info_and_about),
-                 "Prerequisites": preprocess_text(course.General_prereuisites)} for course in all_courses]
-
-df = pd.DataFrame(courses_list)
-# print(df) 
-
 
 # Loading Word2Vec model
-# model = gensim.models.KeyedVectors.load_word2vec_format(r"C:\Users\Simon\proacted\GoogleNews-vectors-negative300.bin", binary=True)
-model = joblib.load(r'C:\Users\Simon\proacted_googleds\word2vec_model.pkl')
+model = gensim.models.KeyedVectors.load_word2vec_format(r"C:\Users\Simon\proacted\GoogleNews-vectors-negative300.bin", binary=True)
 
 
 # these are hashed because they are already stored in the joblib file, no need to initialise them over
-
-# creating 2 vectorizers for course description and prequisites
-vectorizer = TfidfVectorizer(stop_words='english')
-prerequisites_vectorizer = TfidfVectorizer(stop_words='english')
-tfidf_matrix = vectorizer.fit_transform(df['Combined Info'])
-tfidf_matrix_prerequisites = prerequisites_vectorizer.fit_transform(df['Prerequisites'])
-
-feature_names = vectorizer.get_feature_names_out()
-feature_names_for_prerequisites = prerequisites_vectorizer.get_feature_names_out()
-
-# Function to convert text into Word2Vec vectors weighted by TF-IDF scores
-def weighted_vector(text, tfidf_scores, model, feature_names):
-    words = [word for word in text.split() if word in model.key_to_index and word in feature_names]
-    if not words:
-        print(f'\nfor some reason, we dont know what some words you provided mean...\n')
-        return np.zeros(model.vector_size)
-    feature_names = feature_names.tolist()
-    vectors = np.array([model[word] * tfidf_scores[feature_names.index(word)] for word in words])
-    return sum(vectors) / len(vectors)
-
 
 
 # Convert course descriptions and prerequisites to weighted vectors
@@ -79,42 +26,12 @@ course_vectors = np.array([weighted_vector(desc, tfidf_matrix[i].toarray()[0], m
 prerequisites_vectors = np.array([weighted_vector(prereq, tfidf_matrix_prerequisites[i].toarray()[0], model, feature_names_for_prerequisites) for i, prereq in enumerate(df['Prerequisites'])])
 
 # saving the files in joblib, once
-# joblib.dump(vectorizer, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_vectorizer.pkl')
-# joblib.dump(prerequisites_vectorizer, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_prerequisites_vectorizer.pkl')
-# joblib.dump(tfidf_matrix, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix.pkl')
-# joblib.dump(tfidf_matrix_prerequisites, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix_prerequisites.pkl')
-# joblib.dump(course_vectors, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\course_vectors.pkl')
-# joblib.dump(prerequisites_vectors, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\prerequisites_vectors.pkl')
-# print("\n\ndone with storing the pkl files\n")
-
-# loading them from dumped joblib file, everytime the program runs
-tfidf_matrix = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix.pkl')
-vectorizer = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_vectorizer.pkl')
-prerequisites_vectorizer = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_prerequisites_vectorizer.pkl')
-tfidf_matrix_prerequisites = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix_prerequisites.pkl')
-course_vectors = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\course_vectors.pkl')
-prerequisites_vectors = joblib.load(r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\prerequisites_vectors.pkl')
- 
+joblib.dump(vectorizer, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_vectorizer.pkl')
+joblib.dump(prerequisites_vectorizer, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_prerequisites_vectorizer.pkl')
+joblib.dump(tfidf_matrix, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix.pkl')
+joblib.dump(tfidf_matrix_prerequisites, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\tfidf_matrix_prerequisites.pkl')
+joblib.dump(course_vectors, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\course_vectors.pkl')
+joblib.dump(prerequisites_vectors, r'C:\Users\Simon\proacted\AIacademia\trained_models_recommender\prerequisites_vectors.pkl')
+print("\n\ndone with storing the pkl files\n")
 
 
-# embedding student input for interests and subjects, this will come from ui input
-student_vector_for_interests = weighted_vector(user_interests, vectorizer.transform([user_interests]).toarray()[0], model, feature_names)
-student_vector_for_subjects = weighted_vector(user_subjects, prerequisites_vectorizer.transform([user_subjects]).toarray()[0], model, feature_names_for_prerequisites)
-
-
-# Calculate cosine similarities
-interestvscoursedescription_similarities = cosine_similarity([student_vector_for_interests], course_vectors) 
-interestvsprerequisite_similarities = cosine_similarity([student_vector_for_subjects], prerequisites_vectors) 
-combined_total_similarity = (0.65 * interestvscoursedescription_similarities) + (0.35* interestvsprerequisite_similarities)
-
-
-# Get top 5 recommended courses
-top_5_indices = combined_total_similarity[0].argsort()[-5:][::-1] 
-top_5_courses = df['Course Name'].iloc[top_5_indices].tolist() 
-
-
-# printing them, or returning to a view
-print('\n\n\n|-------------------Courses we think would be best for you based on your interests:-----------------|\n\n')
-for course in top_5_courses:
-    print(f'|                  {course}.')
-print('\n\n|----------------------THANKS, THIS IS PRO-ACT-ED 1.1-------------------------------------------------|\n\n\n')
