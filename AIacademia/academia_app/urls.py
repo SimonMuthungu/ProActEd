3a68f436d7c20c6ab9b5d6caaf7e220ad958b847
MerVitz <amakaluvitalis202@gmail.com>
2024-01-18 11:04:03 +0300

    solving erros in views in the main branch

--- AIacademia/academia_app/urls.py
+++ AIacademia/academia_app/urls.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
-from . import admin, views
+from . import views
+from .admin import admin
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.course_recommendation, name='course_recommendation'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("student_page/", views.student_page, name="student_page"),
-    path("admin_page", views.admin_page, name="admin_page"),
-    path("recommend_courses", views.recommend_courses, name="recommend_courses"),
+    path('api/get_courses/<int:school_id>/', views.get_courses, name='get_courses'),
+    path('logout/', LogoutView.as_view(next_page='course_recommendation'), name='logout'),
+    path('course_recommendation/', views.course_recommendation, name='course_recommendation'),
 ]
