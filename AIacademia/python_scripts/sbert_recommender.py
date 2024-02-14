2d120f502f2f832843c45b66f3ff4346010d456c
Simon Muthungu <henrymsechu1@gmail.com>
2024-02-14 06:21:44 +0300

    sbert recommender working well

--- AIacademia/python_scripts/sbert_recommender.py
+++ AIacademia/python_scripts/sbert_recommender.py
@@ -12,8 +12,7 @@ starttime = time.time()
 
 print("Loading s-model")
 local_model_path = os.path.abspath('../../proacted_googleds/sbert_files') 
-print(f"local model path: {local_model_path}")
-print("Contents of the model directory:", os.listdir(local_model_path))
+
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 os.environ["TRANSFORMERS_CACHE"] = local_model_path
 
@@ -35,6 +34,7 @@ try:
     all_sbert_vectors = RecommenderSBERTVectors.objects.all()
 
     combined_scores = []
+    coursescalculatedsimilarityfor = 0
 
     for sbert_vector in all_sbert_vectors:
         course_name = sbert_vector.course_name
@@ -42,8 +42,8 @@ try:
         objectives_embedding_bytes = bytes.fromhex(sbert_vector.objectives_embedding)
 
         # Convert bytes back to NumPy arrays
-        description_embedding_array = np.frombuffer(description_embedding_bytes, dtype=np.float64).reshape(1, -1)
-        objectives_embedding_array = np.frombuffer(objectives_embedding_bytes, dtype=np.float64).reshape(1, -1)
+        description_embedding_array = np.frombuffer(description_embedding_bytes, dtype=np.float32).reshape(1, -1)
+        objectives_embedding_array = np.frombuffer(objectives_embedding_bytes, dtype=np.float32).reshape(1, -1)
 
         # user input embedding
         user_query = "I have a deep interest in health and fitness, focusing on nutrition, exercise, and mental well-being. My goal is to understand the science behind physical fitness and to apply this knowledge in developing holistic health programs. I am keen on exploring the psychological aspects of fitness and how they intersect with physical health, aiming to promote a balanced lifestyle."
@@ -56,6 +56,7 @@ try:
         # Calculate cosine similarity
         objective_similarity = cosine_similarity(userinterest_embedding, objectives_embedding_array)[0][0]
         description_similarity = cosine_similarity(usersenjoyedactivity_embedding, description_embedding_array)[0][0]
+        coursescalculatedsimilarityfor += 1
 
         # Combine the similarities
         combined_similarity = (objective_similarity + description_similarity) / 2
@@ -70,7 +71,8 @@ try:
     N = 5  # Number of top courses you want
     top_course_names = [score[0] for score in combined_scores[:N]]
 
-    print(top_course_names)
+    print(f"top recommended courses: {top_course_names}") 
+    print(f"coursescalculatedsimilarityfor: {coursescalculatedsimilarityfor}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
