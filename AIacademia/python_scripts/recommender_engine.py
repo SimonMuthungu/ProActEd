53c85487e430b724a5a10c81915ebe755d765028
Okeyo <achieng24mercy@gmail.com>
2025-01-31 16:08:57 +0300

    pushing to demonstrate to amol

--- AIacademia/python_scripts/recommender_engine.py
+++ AIacademia/python_scripts/recommender_engine.py
@@ -17,10 +17,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # logging.basicConfig(filename=r'C:\Users\user\Desktop\ProActEd\AIacademia\mainlogfile.log',level=logging.DEBUG, format='%(levelname)s || %(asctime)s || %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
-logging.basicConfig(filename=r'C:\Users\Hp\Desktop\ProActEd\AIacademia\mainlogfile.log',level=logging.DEBUG, format='%(levelname)s || %(asctime)s || %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+# logging.basicConfig(filename=r'C:\Users\Hp\Desktop\ProActEd\AIacademia\mainlogfile.log',level=logging.DEBUG, format='%(levelname)s || %(asctime)s || %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 #logging.basicConfig(filename=r'C:\Users\Simon\proacted\AIacademia\mainlogfile.log',level=logging.DEBUG, format='%(levelname)s || %(asctime)s || %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
-
+logging.basicConfig(filename=r'C:\Users\USER\Desktop\PROACTED\ProActEd\AIacademia\mainlogfile.log',level=logging.DEBUG, format='%(levelname)s || %(asctime)s || %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 # Define a function to preprocess text
 def preprocess_text(text):
