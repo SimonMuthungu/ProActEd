cec1527e54fc1e661701b10f0478fb12a167521d
MerVitz <amakaluvitalis202@gmail.com>
2025-02-04 13:35:14 +0300

    Worked on the rasa, bot

--- AIacademia/rasa-bot/run_rasa_servers.bat
+++ AIacademia/rasa-bot/run_rasa_servers.bat
@@ -1,11 +1,12 @@
 @echo off
 echo Starting Rasa action server...
-start "Rasa Action Server" cmd /k "cd C:\Users\user\Desktop\ProActEd\AIacademia\rasa-bot && rasa run actions --debug"
+cd /d "%~dp0"
+start "Rasa Action Server" cmd /k "rasa run actions --debug"
 
 timeout /t 5 /nobreak > NUL
 
 echo Starting Rasa server...
-start "Rasa Server" cmd /k "cd C:\Users\user\Desktop\ProActEd\AIacademia\rasa-bot && rasa run --enable-api --cors "*" --debug"
+start "Rasa Server" cmd /k "rasa run --enable-api --cors * --debug"
 
 echo Both servers are now running.
 pause
